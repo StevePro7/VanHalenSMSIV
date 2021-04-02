ea0d2a6c3a203e075e57ff21341132271987f873
StevePro7 <steveprozune@hotmail.com>
2021-04-02 19:28:51 +0100

    no message

---
+++ dev/banks/bank16.adb
@@ -0,0 +1,2 @@
+M:bank16
+S:G$RunningDevil_wav_pcmenc$0$0({15054}DA15054d,SC:U),D,0,0
---
+++ dev/banks/bank16.h
@@ -0,0 +1,4 @@
+extern const unsigned char	RunningDevil_wav_pcmenc[];
+#define				RunningDevil_wav_pcmenc_size 15054
+#define				RunningDevil_wav_pcmenc_bank 16
+
