40230bb27767e6d4e016821466a217b80bfe1141
StevePro7 <steveprozune@hotmail.com>
2021-04-02 20:15:38 +0100

    no message

--- dev/screen/test_screen.c
+++ dev/screen/test_screen.c
@@ -1,13 +1,28 @@
 #include "test_screen.h"
 #include "..\engine\enum_manager.h"
 #include "..\engine\font_manager.h"
+#include "..\engine\input_manager.h"
+#include "..\engine\riff_manager.h"
 
 void screen_test_screen_load()
 {
 	engine_font_manager_draw_text( "TEST SCREEN 77", 10, 10 );
+	engine_riff_manager_init();
 }
 
 void screen_test_screen_update( unsigned char *screen_type )
 {
+	unsigned char input1;
+	//unsigned char input2;
+
+	input1 = engine_input_manager_hold( input_type_fire1 );
+	//input2 = engine_input_manager_hold( input_type_fire2 );
+
+	if( input1 )
+	{
+		engine_font_manager_draw_text( "SAMPLE", 10, 15 );
+		engine_riff_manager_play( 0 );
+	}
+
 	*screen_type = screen_type_test;
 }
