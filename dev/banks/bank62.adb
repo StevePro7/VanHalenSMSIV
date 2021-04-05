6adf96cec43d4930fae08c0d7222f6062e2a98c3
StevePro7 <steveprozune@hotmail.com>
2021-04-05 17:42:29 +0100

    no message

--- dev/banks/bank62.adb
+++ dev/banks/bank62.adb
@@ -1,2 +1,2 @@
 M:bank62
-S:G$Riff_2012_03_wav_pcmenc$0$0({13549}DA13549d,SC:U),D,0,0
+S:G$Riff_2012_03_wav_pcmenc$0$0({13577}DA13577d,SC:U),D,0,0
--- dev/banks/bank63.adb
+++ dev/banks/bank63.adb
@@ -1,2 +1,2 @@
 M:bank63
-S:G$Riff_2012_04_wav_pcmenc$0$0({5068}DA5068d,SC:U),D,0,0
+S:G$Riff_2012_04_wav_pcmenc$0$0({8390}DA8390d,SC:U),D,0,0
