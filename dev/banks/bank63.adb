0d4f349765df9c58bbf3b127cb00152c81cec300
StevePro7 <steveprozune@hotmail.com>
2021-04-05 16:44:57 +0100

    no message

--- dev/banks/bank63.adb
+++ dev/banks/bank63.adb
@@ -1,2 +1,5 @@
 M:bank63
-S:G$bank63_txt$0$0({6}DA6d,SC:U),D,0,0
+S:G$Riff_2012_05_wav_pcmenc$0$0({1930}DA1930d,SC:U),D,0,0
+S:G$VanHalen2012__palette__bin$0$0({16}DA16d,SC:U),D,0,0
+S:G$VanHalen2012__tilemap__stmcompr$0$0({106}DA106d,SC:U),D,0,0
+S:G$VanHalen2012__tiles__psgcompr$0$0({9358}DA9358d,SC:U),D,0,0
--- dev/banks/bank63.h
+++ dev/banks/bank63.h
@@ -1,4 +1,16 @@
-extern const unsigned char	bank63_txt[];
-#define				bank63_txt_size 6
-#define				bank63_txt_bank 63
+extern const unsigned char	Riff_2012_05_wav_pcmenc[];
+#define				Riff_2012_05_wav_pcmenc_size 1930
+#define				Riff_2012_05_wav_pcmenc_bank 63
+
+extern const unsigned char	VanHalen2012__palette__bin[];
+#define				VanHalen2012__palette__bin_size 16
+#define				VanHalen2012__palette__bin_bank 63
+
+extern const unsigned char	VanHalen2012__tilemap__stmcompr[];
+#define				VanHalen2012__tilemap__stmcompr_size 106
+#define				VanHalen2012__tilemap__stmcompr_bank 63
+
+extern const unsigned char	VanHalen2012__tiles__psgcompr[];
+#define				VanHalen2012__tiles__psgcompr_size 9358
+#define				VanHalen2012__tiles__psgcompr_bank 63
 
